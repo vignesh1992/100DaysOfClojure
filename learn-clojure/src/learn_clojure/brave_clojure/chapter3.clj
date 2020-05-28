4ea2be0b9e905437cf857171595b89e80c8b97b5
vignesh1992 <ti.vignesh@gmail.com>
2020-05-28 12:28:14 +0530

    Add datastructures of list, vector, map and set

--- learn-clojure/src/learn_clojure/brave_clojure/chapter3.clj
+++ learn-clojure/src/learn_clojure/brave_clojure/chapter3.clj
@@ -56,6 +56,9 @@ failed-protagonist-names
 {:first-name "Charlie"
  :last-name "McFishwich"}
 
+{conj {:first-name "Charlie"
+:last-name "McFishwich"} :test "vignesh"}
+
 (def map_val {:name "vignesh"
               :age "27"})
 
@@ -70,5 +73,51 @@ failed-protagonist-names
 
 ;; NEXT is Keywords ;;
 
+(:age map_val)
+
+;; vectors are index based
+
+([3 2 1] 0)
+
+(get ["a" {:name "Pugsley Winterbottom"} "c"] 1)
+
+;; initialization of vectors
+
+(vector "creepy" "full" "moon")
+
+(conj [1 2 3] 4)
+
+;; List -> we cant use get
+
+'(1 2 3 4)
+
+(nth '(:a :b :c) 0)
+
+(list 1 "two" {3 4})
+
+(conj '(1 2 3) 4)
+
+;; conj adds to the front of the list whereas it adds to back in a vector
+
+;;When should you use a list and when should you use a vector? 
+;;A good rule of thumb is that if you need to easily add items to the beginning of a sequence or if you’re writing a macro, you should use a list. 
+;;Otherwise, you should use a vector. As you learn more, you’ll get a good feel for when to use which.
 
 
+#{"kurt vonnegut" 20 :icicle}
+
+(hash-set 1 1 2 2)
+
+(conj #{:a :b} :b)
+
+(contains? #{:a :b} :a)
+
+(contains? #{:a :b} 3)
+
+(contains? #{nil} nil)
+
+(:a #{:a :b})
+
+(get #{:a :b} :a)
+
+;; NEXT is Functions
