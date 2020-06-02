9ce5d41b6a0846825af19936e27ded4380baabfe
vignesh1992 <ti.vignesh@gmail.com>
2020-06-02 22:06:07 +0530

    Add function

--- learn-clojure/src/learn_clojure/brave_clojure/chapter3.clj
+++ learn-clojure/src/learn_clojure/brave_clojure/chapter3.clj
@@ -285,3 +285,33 @@ failed-protagonist-names
         (recur remaining
                (into final-body-parts
                      (set [part (matching-part part)])))))))
+
+;; matching-part..
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
